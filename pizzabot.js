4aa6d65034d651e5b398363dc084b65683484ca2
frodinger <frej.hedlin@gmail.com>
2023-11-06 22:52:30 +0100

    update

--- pizzabot.js
+++ pizzabot.js
@@ -3,3 +3,9 @@ const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 
 const pizzaPrice = 80;
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+let orderName = prompt('Enter the name of the pizza you want to order today');
+
+let orderQuantity = prompt(`How many of ${orderName} do you want?`);
