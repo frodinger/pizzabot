a784bfc342133bf0873a170266271925a525bc81
frodinger <frej.hedlin@gmail.com>
2023-11-06 23:42:33 +0100

    update

--- pizzabot.js
+++ pizzabot.js
@@ -9,3 +9,6 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 let orderName = prompt('Enter the name of the pizza you want to order today');
 
 let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+let orderTotal = orderQuantity * pizzaPrice;
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`);
