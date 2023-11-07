2e31931875a9b9ebaf6ca25167843684ef67aec1
frodinger <frej.hedlin@gmail.com>
2023-11-07 21:24:05 +0100

    update

--- pizzabot_with_while/pizzabotWhile.js
+++ pizzabot_with_while/pizzabotWhile.js
@@ -4,6 +4,7 @@ const pepperoni = "Pepperoni Pizza";
 
 const pizzaPrice = 80;
 
+// functions
 function checkOrderName(pizzaName) {
     return pizzaName.toLowerCase() == vegetarian.toLowerCase() || pizzaName.toLowerCase() == hawaii.toLowerCase() 
     || pizzaName.toLowerCase() == pepperoni.toLowerCase() || pizzaName.toLowerCase() == "vegetarian".toLowerCase() 
@@ -22,28 +23,48 @@ function cookingTime(orderQuant){
     return cookTime;
 }
 
+function totalCost(orderQuant) {
+    let orderTotal = orderQuant * pizzaPrice;
+    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. ${cookingTime(orderQuant)}`);
+} 
 
-function orderPizza() {
-    alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaii} and ${pepperoni}`);
-    
+// koden
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaii} and ${pepperoni}`);
+
+let correctOrder = false;
+
+while (!correctOrder) {
     let orderName = prompt('Enter the name of the pizza you want to order today');
-    
-    if (!checkOrderName(orderName)) {
+
+    correctOrder = checkOrderName(orderName);
+    if (!correctOrder) {
         alert("That pizza isn't on the menu");
     }
-    else {
-        let orderQuantity = prompt(`How many of the ${orderName} do you want?`);
+}
 
-        if (orderQuantity < 1) {
-            alert("You can't order less than one pizza");
-        } else {
-            function totalCost(orderQuant) {
-                let orderTotal = orderQuant * pizzaPrice;
-                alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. ${cookingTime(orderQuant)}`);
-            } 
-            
-            totalCost(orderQuantity);
-        }
-        
+let orderQuantity = prompt(`How many of the ${orderName} do you want?`);
+
+totalCost(orderQuantity);
+
+/*
+
+function checkIfValidQuantity(quant) {
+    return quant >= 1;
+}
+
+let validQuantity = false;
+
+while (!validQuantity) {
+    let orderQuantity = prompt(`How many of the ${orderName} do you want?`);
+    
+    validQuantity = checkIfValidQuantity(orderQuantity);
+    if (!validQuantity) {
+        alert("You can't order less than one pizza");
     }
 }
+
+
+
+totalCost(orderQuantity);
+*/
