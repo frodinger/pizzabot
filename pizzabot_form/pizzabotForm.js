ddd52a63983e22088047247d4e83fefa99983133
frodinger <frej.hedlin@gmail.com>
2023-11-09 22:01:54 +0100

    nu funkar det

--- pizzabot_form/pizzabotForm.js
+++ pizzabot_form/pizzabotForm.js
@@ -1,10 +1,3 @@
-/*
-Jag får det inte att fungera men här är iaf mitt försök
-*/
-const vegetarian = "Vegetarian Pizza";
-const hawaiian = "Hawaiian Pizza";
-const pepperoni = "Pepperoni Pizza";
-
 const pizzaPrice = 80;
 
 const pizzaType = document.getElementById("pizzaType");
@@ -12,33 +5,18 @@ const pizzaQuantity = document.getElementById("pizzaQuantity");
 const orderBtn = document.getElementById("orderBtn");
 const orderTextEl = document.getElementById("orderText");
 
-const orderName = pizzaType.value;
-const orderQuantity = pizzaQuantity.value;
-const orderTotal = orderQuantity * pizzaPrice;
-
-
-// funktioner
-function cookingTime(orderQuant){
-    let cookTime = "";
-    if (orderQuant <= 2) {
-        cookTime = "The pizzas will take 10 minutes.";
-    } else if (orderQuant <= 5) {
-        cookTime = "The pizzas will take 15 minutes.";
-    } else if (orderQuant >= 6) {
-        cookTime = "The pizzas will take 20 minutes.";
+orderBtn.addEventListener("click",(e) => {
+    const orderName = pizzaType.value;
+    const orderQuantity = pizzaQuantity.value;
+    const orderTotal = orderQuantity * pizzaPrice;
+    let cookingTime;
+  
+    if (orderQuantity <= 2) {
+      cookingTime = 10;
+    } else if (orderQuantity <= 5) {
+      cookingTime = 15;
+    } else if (orderQuantity >= 6) {
+      cookingTime = 20;
     }
-    return cookTime;
-}
-
-orderBtn.addEventListener("click", (e) => {
-    const orderText = `Great, I'll get started on your ${orderName} right away. ${cookingTime(orderQuantity)} It will cost ${orderTotal} kr`;
-    orderTextEl.textContent = orderText;
-    /*
-    if (orderQuantity < 1) {
-        orderTextEl.textContent = "You can't order less than 1 pizza";
-    } else {
-        const orderText = `Great, I'll get started on your ${orderName} right away. ${cookingTime(orderQuantity)} It will cost ${orderTotal} kr`;
-        orderTextEl.textContent = orderText;
-    }
-    */
+    orderTextEl.textContent = `Great, I'll get started on your ${orderName} right away. The pizzas will take ${cookingTime} minutes. It will cost ${orderTotal} kr`;
 });
